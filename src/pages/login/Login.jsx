a35a3987a6897d8a090c880168a70af1309457af
thadeus <thadeusnyariki94@gmail.com>
2023-02-03 15:41:10 +0300

    first commit

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './login.scss'
+
+const Login = () => {
+  return (
+    <div className='login'>
+      <div className="card">
+        <div className="left">
+          <h1>Hello World</h1>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam reprehenderit dolorum voluptates!</p>
+          <span>Don't you have an account?</span>
+          <button>Register</button>
+        </div>
+        <div className="right"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
